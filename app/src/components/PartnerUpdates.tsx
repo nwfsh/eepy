b1c6ee856d8a7201a47a6356c06d22c844f5212c
cxinyu14 <cxinyu14@student.ubc.ca>
2026-02-25 03:24:46 -0800

    feat: implementing types needed for streaks

---
+++ app/src/components/PartnerUpdates.tsx
@@ -0,0 +1,4 @@
+
+
+// imports from logic if partner opens the app or not correct
+// import logic of their TIME use TIME FROM LOGIC
---
+++ app/src/logic/streak.tsx
@@ -0,0 +1,5 @@
+
+
+
+// import the couple to find their logic of streak
+// each user has their own streak, but couple streak only goes up when both goes up
