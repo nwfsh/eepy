1750c3195b8be9ff2d9ec317f54c07b8b613b96a
cxinyu14 <cxinyu14@student.ubc.ca>
2026-07-28 23:36:49 -0700

    feat: set up React Navigation

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,10 +1,15 @@
 // entry point to the app 
-
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { AuthProvider } from "./src/context/AuthContext";
 import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { useFonts, Inter_500Medium, Inter_400Regular, Inter_600SemiBold,Inter_700Bold } from "@expo-google-fonts/inter";
+
+const Stack = createStackNavigator();
 
 export default function App() {
 
@@ -17,13 +22,16 @@ export default function App() {
 
     if (!fontsLoaded) return null;
 
-  return (
+return (
     <AuthProvider>
-    <View style={styles.container}>
-        <LoginScreen/>
-    </View>
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="LogIn" component={LoginScreen} options = {{ animation : "none"}} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} options = {{ animation: "none"}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     </AuthProvider>
-  );
+);  
 }
 
 const styles = StyleSheet.create({
---
+++ frontend/app/src/screens/SignUpScreen.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert,} from "react-native";
+import { supabase} from "../lib/supabase"
+import { onBoardingStyles } from "../styles/onboarding";
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function SignUp({navigation}: any) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSignUp =  async () => {
+        setLoading(true);
+        if (password !==  confirmPassword) {
+            Alert.alert("Error","Password does not match");
+            setLoading(false);
+            return;
+        }
+        const { error } = await supabase.auth.signUp({
+            email, password});
+
+        if (error) {
+            Alert.alert("Error",error.message)
+        }
+
+        setLoading(false);
+    };
+
+    return (
+        <View style = {onBoardingStyles.container}> 
+        <View style={{ position: 'absolute', top: 160, left: 0 }}>
+        <Image source={require("../assets/onboardingsun.png")} style={{ width: 200, height: 150 }} />
+        </View>
+        <Text style = { onBoardingStyles.title }> Sign Up</Text>
+        <Text style = { onBoardingStyles.subtitle}> Stay Connected With Eepy </Text>
+
+        <Text style = {onBoardingStyles.inputTitle}> Email </Text>
+        <TextInput style = { onBoardingStyles.inputField}
+        value = {email} 
+        onChangeText={setEmail}
+        // tells your keyboard to add @ into the keyboard makes it easier 
+        keyboardType="email-address"
+        autoCapitalize="none"
+        placeholder="your@gmail.com"
+        />
+
+        <Text style = {onBoardingStyles.inputTitle} > Password </Text>
+        <TextInput style = { onBoardingStyles.inputField}
+        value = {password}
+        onChangeText = {setPassword}
+        // hide whats you type
+        secureTextEntry 
+        placeholder = "password"
+        />
+
+        <Text style = {onBoardingStyles.inputTitle} > Confirm Password </Text>
+        <TextInput style = { onBoardingStyles.inputField}
+        value = {confirmPassword}
+        onChangeText = {setConfirmPassword}
+        // hide whats you type
+        secureTextEntry 
+        placeholder = "confirm password"
+        />
+
+        
+        <TouchableOpacity style = {onBoardingStyles.finalButton} onPress={handleSignUp} disabled={loading}>
+            <Text style = { onBoardingStyles.buttonText}> {loading ? "Signing Up..." : "Sign Up" }</Text>
+        </TouchableOpacity>
+        
+
+        <Text style = {onBoardingStyles.linkText }> 
+        Already have an account?{" "}
+            <Text style = {onBoardingStyles.linkBold}
+            onPress={ () => navigation.navigate("LogIn")}
+            >Sign In</Text>
+        </Text>
+
+        <View style={{ position: 'absolute', top: 700, right: 0}}>
+        <Image source={require("../assets/onboardingmoon.png")} style={{ width: 200, height: 80}} />
+        </View>
+
+        </View>
+
+    )
+}
